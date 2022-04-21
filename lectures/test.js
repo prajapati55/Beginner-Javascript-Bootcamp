fa5f1cb0969ac604118659324fd2a63a4b000c14
Pravendra Kumar <prvndrkumar55@gmail.com>
2022-04-21 12:27:04 +0530

    lecture 5 part2

---
+++ lectures/test.js
@@ -0,0 +1,13 @@
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+console.log(plants.pop());
+// expected output: "tomato"
+
+console.log(plants);
+// expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+// plants.pop();
+plants.push("cornflower");
+
+console.log(plants);
+console.log("length", plants.length);
