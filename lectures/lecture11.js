736013705717227a479a22e7406a7e195545e58b
Pravendra Kumar <kumar.pravendra@thinksys.com>
2022-06-01 18:28:57 +0530

    update lecture name

---
+++ lectures/lecture11.js
@@ -0,0 +1,15 @@
+/**
+  sort an array
+ */
+// expected o/p = [2,4,5,6,7,8]
+//   const numbers = [2,5,8,7,4,6];
+//   for(let i=0; i<numbers.length; i++){
+//       for(j=i+1; j<numbers.length; j++){
+//             if(numbers[i] < numbers[j]){
+//                 let val = numbers[j];
+//                 numbers[j] = numbers[i];
+//                 numbers[i] = val;
+//             }
+//       }
+//   }
+// console.log("numbers", numbers);
